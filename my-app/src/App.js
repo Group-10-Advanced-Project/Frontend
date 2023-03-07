265333047404ae61df8ca3ca38a149a569f7638e
Belal Khaleel <belalkhaleel19@gmail.com>
2023-03-07 15:47:02 +0200

    setup routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
 import { BrowserRouter } from 'react-router-dom';
-import AllRoutes from './components/allRoutes';
+import Admin from "./pages/admin/admin.js";
+import Dashboard from "./pages/dashboard/dashboard.js";
+import Employee from "./pages/employees/employee.js";
+import Login from "./pages/login/login.js";
+import Teams from "./pages/teams&projects/teams-projects.js";
+import NewAdmin from "./pages/admin/create-admin.js";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <AllRoutes/>
+      <Routes>
+      <Route path="/" element={<Dashboard/>}/>
+                <Route path="/admin" element={<Admin/>}/>
+                <Route path="/employee" element={<Employee/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/team&projects" element={<Teams/>}/>
+                <Route path="/newAdmin" element={<NewAdmin/>}/>
+                </Routes>
       </BrowserRouter>
     </div>
   );
