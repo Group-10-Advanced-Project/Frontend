55876d53bf36aa820925861c6b50bd7221d6b51d
Belal Khaleel <belalkhaleel19@gmail.com>
2023-03-08 16:40:40 +0200

    added search bar

--- my-app/src/components/header/index.js
+++ my-app/src/components/header/index.js
@@ -1,11 +1,36 @@
 import "./header.css";
+import React, { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
-function Header (props){
-    return(
-        <div>
-            <h1>Header</h1>
-        </div>
-    )
-}
+const Header = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+  return (
+    <div className="header">
+      <form className="search-form">
+        <fieldset className="search-fieldset">
+          <input
+            type="text"
+            name=""
+            placeholder="Search"
+            onChange={handleChange}
+            value={searchInput}
+            className="search-bar"
+          />
+          <button className="search-btn" type="submit">
+            <AiOutlineSearch />
+          </button>
+          {/* <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          ></link> */}
+        </fieldset>
+      </form>
+    </div>
+  );
+};
 
 export default Header;
--- my-app/src/pages/dashboard/dashboard.js
+++ my-app/src/pages/dashboard/dashboard.js
@@ -1,13 +1,13 @@
 import "./dashboard.css";
+import Header from "../../components/header";
 
-function Dashboard (props) {
-    return (
-        <div>
-            <h1>
-                Dashboard
-            </h1>
-        </div>
-    );
+function Dashboard(props) {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Header />
+    </div>
+  );
 }
 
 export default Dashboard;
