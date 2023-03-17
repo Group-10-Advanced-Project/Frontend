bb3aa62099725c3b8121cdd765633f44160dcfae
Ahmad  Kharma <ahmad2003kharma@gmail.com>
2023-03-17 21:52:23 +0200

    tabel employee

--- my-app/src/pages/employees/employee.js
+++ my-app/src/pages/employees/employee.js
@@ -1,62 +1,20 @@
 import "./employee.css";
 
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 
-import Card from "./Card";
-// import photo from "./ahmad.jpeg";
+import DataGridDemo from "./employeeTable.js";
+
 
 function Employee(props) {
-  
-  const [employees, setEmployees] = useState([]);
-  useEffect(() => {
-    axios
-      .get("http://localhost:8000/api/employee")
-      .then((res) => {
-        console.log(res.data);
-        setEmployees(res.data);
-       
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [employees]);
-  const handleDelete = (employee_id) => {
-    axios
-      .delete(`http://localhost:8000/api/employee/${employee_id}`)
-      .then(() => {
-        
-        // const updatedEmployees = employees.filter((employee) => employee.employee_id !== employee_id);
-        // setEmployees(updatedEmployees);
-      window.location.reload(true)
-      })
-      .catch((err) => {
-        console.log(err);
-        
-      });
-  };
-  return !employees ? (
-    <h1>no employee</h1>
-  ) : (
+  return (
     <div className="main center">
-      {employees.map((employee) => (
-        <Card
-          key={employee.id}
-          picture={employee.picture}
-          employee_id={employee.employee_id}
-          email={employee.email}
-          first_name={employee.first_name}
-          last_name={employee.last_name}
-          team_id={employee.team_id}
-          phone_number={employee.phone_number}
-          onDelete={handleDelete}
-        />
-      ))}
+      <h1 className="employeeTitle">Employee</h1>
+   
+      <div className="employeeTable">
+        <DataGridDemo></DataGridDemo>
+      </div>
+    
     </div>
   );
-  
-
 }
 export default Employee;
-
-
